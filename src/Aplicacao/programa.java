d5d199dc18cfc8c4d21045f79bc126da0f03a3c7
charlesmagalhaes <charlesrm29@gmail.com>
2021-02-01 00:49:59 -0300

    Projeto Teste

---
+++ src/Aplicacao/programa.java
@@ -0,0 +1,54 @@
+package Aplicacao;
+
+import java.util.Scanner;
+
+import Entidades.Clientes;
+
+
+public class programa {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		Clientes[] vetor = new Clientes[10];
+		
+		
+		System.out.print("Quantos quartos serão alugados: ");
+		int reservas = sc.nextInt();
+		
+		for (int i=0; i<reservas;i++) {
+			System.out.println("Rent #"+i);
+			
+			System.out.print("Nome: ");
+			String nome= sc.next();
+			
+			System.out.print("email: ");
+			String email= sc.next();
+			
+			System.out.print("Quarto: ");
+			int posicao = sc.nextInt();
+			
+			vetor[posicao] = new Clientes(nome, email);
+			
+			System.out.println();
+			
+			
+		}
+		
+		
+		for (int i=0; i<vetor.length;i++)
+		
+			if (vetor[i]!= null) {
+				
+				System.out.println("Quartos reservados:");
+				System.out.println(i+": "+vetor[i].nome+", "+vetor[i].email);
+			}
+		
+		
+		sc.close();
+
+	}
+
+}
